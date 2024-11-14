399dcc419848dde0525eb45d556a6171c8e02d6c
al.larin <al.larin1308@gmail.com>
2024-11-14 22:26:02 +0300

    create profile table

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,7 +1,8 @@
-__all__ = ("Base", "Product", "db_manager", "User")
+__all__ = ("Base", "Product", "db_manager", "User", "Post", "Profile")
 
 from .base import Base
 from .product import Product
 from .db_manager import db_manager
 from .user import User
 from .post import Post
+from .profile import Profile
