4e65f0a02a850cc6c67b86f2455c9a34036e5f0a
al.larin <al.larin1308@gmail.com>
2024-11-14 19:37:22 +0300

    create user model

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,5 +1,6 @@
-__all__ = ("Base", "Product", "db_manager")
+__all__ = ("Base", "Product", "db_manager", "User")
 
 from .base import Base
 from .product import Product
 from .db_manager import db_manager
+from .user import User
