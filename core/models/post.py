6e913f95cca13fa88127967181af7a2015737c37
al.larin <al.larin1308@gmail.com>
2024-11-14 22:26:29 +0300

    create user mixin

--- core/models/post.py
+++ core/models/post.py
@@ -4,15 +4,17 @@ from sqlalchemy import String, Text, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
+from .mixins import UserRelationMixin
+
+# if TYPE_CHECKING:
+#     from .user import User
 
 
-if TYPE_CHECKING:
-    from .user import User
+class Post(UserRelationMixin, Base):
+    _user_back_populates = "posts"
 
-
-class Post(Base):
     title: Mapped[str] = mapped_column(String(100), nullable=False)
     body: Mapped[str] = mapped_column(Text, default="", server_default="")
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
 
-    user: Mapped["User"] = relationship(back_populates="posts")
+    # user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    # user: Mapped["User"] = relationship(back_populates="posts")
