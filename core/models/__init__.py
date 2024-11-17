a61ceb02b5ea698eb9e7c14f560c060df0777efd
al.larin <al.larin1308@gmail.com>
2024-11-17 09:21:57 +0300

    create product order association table

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,4 +1,13 @@
-__all__ = ("Base", "Product", "db_manager", "User", "Post", "Profile")
+__all__ = (
+    "Base",
+    "Product",
+    "db_manager",
+    "User",
+    "Post",
+    "Profile",
+    "Order",
+    "order_product_association_table",
+)
 
 from .base import Base
 from .product import Product
@@ -6,3 +15,5 @@ from .db_manager import db_manager
 from .user import User
 from .post import Post
 from .profile import Profile
+from .order import Order
+from .order_product_association import order_product_association_table
